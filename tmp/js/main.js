509576f57e2adf3c620aeedb65faa436645e893e
Xeselaro <sese.tailor@gmail.com>
2015-12-03 19:11:26 +0100

    Refactor code

--- tmp/js/main.js
+++ tmp/js/main.js
@@ -1,14 +1,3 @@
-Polymer({
-	is: 'tmp-map',
-
-	ready: function(){
-		generateMap();
-		mapTo_16_9();
-	}
-});
-
-var blocks = [];
-
 function l(s){
 	console.log(s);
 }
@@ -17,14 +6,4 @@ function getRandomBoolean(){
 }
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function mapTo_16_9(){
-	if(((window.innerWidth / 16) * 9) < window.outerHeight){
-		$("map").height(((window.innerWidth / 16 ) * 9));
-		$("map").css("top", (window.outerHeight - ((window.innerWidth / 16) * 9)) / 3); 
-	}
-	if(((window.outerHeight / 9) * 16) < window.innerWidth){
-		$("map").width(((window.outerHeight / 9 ) * 16));
-		$("map").css("left", (window.innerWidth - ((window.outerHeight / 9) * 16)) / 2);
-	}
 }
