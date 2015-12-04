bae625d1a5fc15c7d86bc34886f3418545f22438
Dennis Kugelmann <kugelmann.dennis@gmail.com>
2015-12-04 21:42:39 +0100

    Create a new Global game

--- tmp/js/main.js
+++ tmp/js/main.js
@@ -7,3 +7,12 @@ function getRandomBoolean(){
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function generateGameId(length) {
+	var text = '';
+	var pool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (var i = 0; i < length; i++) {
+		text += pool.charAt(Math.floor(Math.random() * pool.length));
+	}
+	return text;
+}
